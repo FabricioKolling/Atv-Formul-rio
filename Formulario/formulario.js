b1de19cc0ab5c83d872e0ebcac18a17c35a4458f
Fabricio Kolling <162700727+FabricioKolling@users.noreply.github.com>
2024-07-08 09:57:39 -0300

    Add files via upload

---
+++ Formulario/formulario.js
@@ -0,0 +1,14 @@
+function EnviarDados(){
+
+    let user = document.getElementById('NomeUser').value
+    let dataNascimento = document.getElementById('IdadeUser').value
+    let email = document.getElementById('EmailUser').value
+    let senha = document.getElementById('SenhaUser').value
+    let notificar = document.querySelector("input[name='sim_ou_nao']:checked").value
+
+
+    alert(`Seu nome é: ${user} \nSua data de nascimento é: ${dataNascimento} \nSeu email é: ${email} \nSua senha registrada: ${senha}\nVocê quer receber notificações: ${notificar}`)
+
+    window.location.href='index.html'
+
+}
